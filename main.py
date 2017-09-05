4bc008c89a86e647d8a773ec6f43e55ca708c813
Obed N Munoz <obed.n.munoz@gmail.com>
2017-09-05 15:28:12 -0500

    Add cmake support
    This commit add cmake support. Cmake automatically looks for
    OpenCV dependencies and then generate the proper Makefile.
    
    Signed-off-by: Obed N Munoz <obed.n.munoz@gmail.com>

--- main.py
+++ main.py
@@ -3,11 +3,11 @@
 import time
 import os
 start_time = time.time()
-os.system("./discrete_fourier_transform")
-os.system("./contours2")
-os.system("./edge")
-os.system("./smoothing")
-os.system("./morphology_2")
+os.system("./bin/discrete_fourier_transform")
+os.system("./bin/contours2")
+os.system("./bin/edge")
+os.system("./bin/smoothing")
+os.system("./bin/morphology_2")
 
 print
 print("Benchmark took  %s seconds" % (time.time() - start_time))
